aa3c9f473565340ee70fa3fa710bd138de1406b8
vangona <rlarhksrud12@naver.com>
2021-05-13 23:48:55 +0900

    Update bg.js

--- js/bg.js
+++ js/bg.js
@@ -4,7 +4,7 @@ const IMG_NUMBER = 17;
 
 function paintImage(imgNumber){
     const image = new Image();
-    image.src = `https://cdn.jsdelivr.net/gh/vangona/js-basic-clock/images/${imgNumber + 1}.jpg`;
+    image.src = `./images/${imgNumber + 1}.jpg`;
     image.classList.add("bgImage");
     body.prepend(image);
 }
