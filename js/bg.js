116600c15c7f024c6c18a0aaf1f948935bd2298d
vangona <rlarhksrud12@naver.com>
2026-02-06 13:47:52 +0900

    feat: 명언 추가, 세로형일 때 슬라이드

--- js/bg.js
+++ js/bg.js
@@ -6,6 +6,18 @@ function paintImage(imgNumber){
     const image = new Image();
     image.src = `./images/${imgNumber + 5}.jpg`;
     image.classList.add("bgImage");
+
+    image.onload = function() {
+        // 이미지가 가로형인지 확인 (가로가 세로보다 큰 경우)
+        const isLandscapeImage = image.naturalWidth > image.naturalHeight;
+        // 화면이 세로형인지 확인 (세로가 가로보다 큰 경우)
+        const isPortraitScreen = window.innerHeight > window.innerWidth;
+
+        if (isLandscapeImage && isPortraitScreen) {
+            image.classList.add("bgImage--panning");
+        }
+    };
+
     body.prepend(image);
 }
 
